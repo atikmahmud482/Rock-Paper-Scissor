6044dc9733bd896c9227d30716b606c4fd4ccf5b
atikmahmud482 <atikmahmud482@gmail.com>
2025-01-12 19:44:08 +0600

    finished

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ let compScore = 0;
 const choices = document.querySelectorAll(".choice");
 const msg = document.querySelector("#msg");
 
+const userScorePara = document.querySelector("#user-score");
+const compScorePara = document.querySelector("#comp-score");
+
 const genCompChoice = () => {
   const options = ["rock", "paper", "scissors"];
   const randomIdx = Math.floor(Math.random() * 3);
@@ -11,28 +14,26 @@ const genCompChoice = () => {
 };
 
 const drawGame = () => {
-  //   console.log("game was draw");
   msg.innerText = "Game was draw.";
   msg.style.backgroundColor = "black";
 };
 
-const showWinner = (userWin) => {
+const showWinner = (userWin, userChoice, compChoice) => {
   if (userWin) {
-    // console.log("you win");
-    msg.innerText = "You Win!";
+    userScore++;
+    userScorePara.innerText = userScore;
+    msg.innerText = `You Win! ${userChoice} beats ${compChoice}`;
     msg.style.backgroundColor = "green";
   } else {
-    // console.log("you loos");
-    msg.innerText = "You Lose.";
+    compScore++;
+    compScorePara.innerText = compScore;
+    msg.innerText = `You Lose. ${compChoice} beats your ${userChoice}`;
     msg.style.backgroundColor = "red";
   }
 };
 
 const playGame = (userChoice) => {
-  //   console.log("user choice", userChoice);
-
   const compChoice = genCompChoice();
-  //   console.log("comp choice", compChoice);
 
   if (userChoice === compChoice) {
     drawGame();
@@ -45,16 +46,13 @@ const playGame = (userChoice) => {
     } else {
       userWin = compChoice === "rock" ? false : true;
     }
-    showWinner(userWin);
+    showWinner(userWin, userChoice, compChoice);
   }
 };
 
 choices.forEach((choices) => {
-  //   console.log(choices);
   choices.addEventListener("click", () => {
-    // console.log("choice was clicked");
     const userChoice = choices.getAttribute("id");
     playGame(userChoice);
-    // console.log("i am", userChoice);
   });
 });
