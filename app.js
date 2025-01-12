54271ad8d0cf4439a3f579b47ac3eccf7d44d6a0
atikmahmud482 <atikmahmud482@gmail.com>
2025-01-12 19:05:12 +0600

    html,css & some java add

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-console.log("hi");
+let userScore = 0;
+let compScore = 0;
+
+const choices = document.querySelectorAll(".choice");
+
+const genCompChoice = () => {
+  const options = ["rock", "paper", "scissors"];
+  const randomIdx = Math.floor(Math.random() * 3);
+  return options[randomIdx];
+};
+
+const playGame = (userChoice) => {
+  const compChoice = genCompChoice();
+};
+
+choices.forEach((choices) => {
+  //   console.log(choices);
+  choices.addEventListener("click", () => {
+    // console.log("choice was clicked");
+    const userChoice = choices.getAttribute("id");
+    playGame(userChoice);
+    console.log("i am", userChoice);
+  });
+});
